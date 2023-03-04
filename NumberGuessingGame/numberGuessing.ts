2806bc19be6fc0e10495706cd7d773c791e5405d
Khaula Azhar <78435584+KhaulaAzhar@users.noreply.github.com>
2023-03-04 22:45:09 +0500

    Add files via upload

---
+++ NumberGuessingGame/numberGuessing.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+import inquirer from 'inquirer';
+
+type anstype = {
+    userGuess: number
+}
+const systemGeneratedNo = Math.floor(Math.random() *10);
+
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "write your guess: "
+    }
+])
+const {userGuess} = answers
+console.log(userGuess, "userGuess", systemGeneratedNo, 'SYs')
+if(userGuess === systemGeneratedNo){
+
+    console.log("yeaaa your answer is correct \n You win")
+} else {
+    console.log("Please try again better luck next time!")
+}
